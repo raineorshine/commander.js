a91c2bfbc697c49a6eb354876f882f00582352c1
Raine Revere <raine@cybersemics.org>
2023-03-29 13:11:39 -0400

    Update configure-help.js

--- examples/configure-help.js
+++ examples/configure-help.js
@@ -5,10 +5,13 @@ const program = new commander.Command();
 
 // This example shows a simple use of configureHelp.
 // This is used as an example in the README.
+// Any method on the Help class can be overridden
+// See: https://github.com/tj/commander.js/blob/master/lib/help.js
 
 program.configureHelp({
   sortSubcommands: true,
   subcommandTerm: (cmd) => cmd.name() // Just show the name, instead of short usage.
+  
 });
 
 program.command('zebra <herd-size>', 'African equines with distinctive black-and-white striped coats');
